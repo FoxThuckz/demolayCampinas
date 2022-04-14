03e0dc72af54908a2c062e07b4c6f3b15abc0dc1
Pedro Garcia <pedroengarcia@gmail.com>
2022-04-13 21:54:47 -0300

    Teste

--- appDemolay.js
+++ appDemolay.js
@@ -2,11 +2,12 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 
-const PORT = process.env.PORT || 8877;
+const PORT = process.env.PORT || 8878;
 
-app.get('/contato',(req, res)=>{
-    res.redirect('https://contate.me/CampinasDemolay');
+// app.get('/contato',(req, res)=>{
+//     res.redirect('https://contate.me/CampinasDemolay');
+// })
+
+app.listen(PORT,()=>{
+    console.log('Escutandoi'+ PORT);
 })
-
-app.listen(PORT,()=>{console.log('Escutando porta: '+ PORT);
- })
